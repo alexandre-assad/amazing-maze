43867ca8875e5441ab78c3e6f2023e1db7e9518b
Alexandre Assad <alexandre.assad@laplateforme.io>
2023-09-08 12:36:36 +0200

    [ADD] Init Case class

---
+++ Materials/Case.py
@@ -0,0 +1,9 @@
+
+
+class Case:
+
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.visited = False
+        self.walls = {"Top":True,"Left":True,"Right":True,"Bottom":True}
