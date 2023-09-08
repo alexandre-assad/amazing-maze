05792f70f5eb1566be4d67f4af0d24aed5ffb23b
Alexandre Assad <alexandre.assad@laplateforme.io>
2023-09-08 13:31:41 +0200

    [ADD] Test Labyrinth wall_break()

--- Materials/Labyrinth.py
+++ Materials/Labyrinth.py
@@ -9,6 +9,10 @@ class Labyrinth:
 
 
     def setup(self):
+        """
+        Input : self
+        Output : a double matrix, the board of Cases
+        """
         board = []
         sub_board = []
         for li in range(self.length):
@@ -16,4 +20,5 @@ class Labyrinth:
                 sub_board.append(Case(li,col))
             board.append(sub_board)
             sub_board = []
+        #The board is closed, so now we can see it as a box of little closed box, to create the labyrinth we'll destroy Case.wall
         return board
--- Test/Materials/Labyrinth_test.py
+++ Test/Materials/Labyrinth_test.py
@@ -9,3 +9,15 @@ class TestLabyrinth(unittest.TestCase):
         self.assertEqual(Lab1.board[0][0].visited,False)
         self.assertEqual([Lab1.board[2][3].x,Lab1.board[2][3].y],[2,3])
         self.assertEqual(Lab1.board[4][7].walls["Top"],True)
+
+
+    def test_wall_break(self):
+        Lab1 = Labyrinth(3)
+        Lab1.wall_break("Right",0,0)
+        self.assertEqual([Lab1.board[0,0].walls["Right"],Lab1.board[0][1]["Left"]],[False,False])
+
+
+
+    def test_display(self):
+        Lab1 = Labyrinth(3)
+        pass
