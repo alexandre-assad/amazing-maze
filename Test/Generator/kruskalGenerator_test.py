e7cda71ef082f28247f3d7f14f0e17958046c6ce
Alexandre Assad <alexandre.assad@laplateforme.io>
2023-09-11 09:27:32 +0200

    [ADD] Test kruskalGenerator()

---
+++ Test/Generator/kruskalGenerator_test.py
@@ -0,0 +1,15 @@
+import unittest 
+from Generator.kruskalGenerator import *
+from Utils.txt_utils import *
+from Utils.utils import *
+
+class TestKruskalGenerator(unittest.TestCase):
+
+    def test_backtrackGenerator(self):
+        kruskalGenerator(3,"kruskal_lab_test.txt")
+        self.assertEqual(read_txt("kruskal_lab_test.txt")[0:2],"\n#")
+        Lab1 = txt_to_labyrinth("kruskal_lab_test.txt")
+        self.assertEqual(Lab1.board[0][0].walls["Top"],True)
+
+
+        
