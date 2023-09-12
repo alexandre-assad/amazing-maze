d4bb6447cf4750efe2867e4c40e75fd3af60db35
Alexandre Assad <alexandre.assad@laplateforme.io>
2023-09-12 09:02:24 +0200

    [ADD] Test backtrackResolver()

--- Test/Resolver/backtrackResolver_test.py
+++ Test/Resolver/backtrackResolver_test.py
@@ -1,9 +1,18 @@
 import unittest
 from Resolver.backtrackResolver import *
 from Materials.Labyrinth import Labyrinth
+from Utils.utils import *
 
 
 class TestBacktrackResolver(unittest.TestCase):
 
     def test_backtrackResolver(self):
-        pass
+        Lab1 = txt_to_labyrinth("lab3")
+        self.assertEqual(Lab1.displayResolver(),"""
+# #####
+#o o#*#
+### # #
+#*#o o#
+# ### #
+#* * o
+#######""")
