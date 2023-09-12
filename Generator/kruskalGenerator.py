73a571fbd4496e5aed33fe0d62a2c9b4879c8445
Alexandre Assad <alexandre.assad@laplateforme.io>
2023-09-12 08:32:52 +0200

    [MAJOR FIX] Kruskal Generator Algorithm

--- Generator/kruskalGenerator.py
+++ Generator/kruskalGenerator.py
@@ -4,29 +4,36 @@ from Materials.Labyrinth import Labyrinth
 from Utils.os_utils import *
 from Utils.txt_utils import *
 
-def kruskalGenerator(len:int,file:str) -> Labyrinth:
-    Lab1 = Labyrinth(len)
+def kruskalGenerator(length:int,file:str) -> Labyrinth:
+    Lab1 = Labyrinth(length)
     case_arround = Lab1.case_number_arround(0,0)
     random.shuffle(case_arround)
-    Lab1 = propagation(Lab1,[0,0],case_arround[0])
-    while Lab1.krustal_cell["0"].len < Lab1.length**2:
-        x,y = random.randint[0,Lab1.length-1],random.randint[0,Lab1.length-1]
+    propagation(Lab1,[0,0],case_arround[0])
+    while len(Lab1.kruskal_cell["0"]) < Lab1.length**2:
+        x,y = random.randint(0,Lab1.length-1),random.randint(0,Lab1.length-1)
         case_arround = Lab1.case_number_arround(x,y)
-        random.shuffle(case_arround)        
-        Lab1 = propagation([0,0],case_arround[0])
+        if case_arround != []:
+            random.shuffle(case_arround)        
+            propagation(Lab1,[x,y],case_arround[0])
     write_txt(Lab1.display(),file)
     return Lab1
 
 def propagation(laby:Labyrinth,pos1:list,pos2:list) -> Labyrinth:
+
     laby.wall_break(getDirection(pos1[0],pos1[1],pos2[0],pos2[1]),pos1[0],pos1[1])
+
     number = min(laby.board[pos1[0]][pos1[1]].k_number,laby.board[pos2[0]][pos2[1]].k_number)
     other_number = max(laby.board[pos1[0]][pos1[1]].k_number,laby.board[pos2[0]][pos2[1]].k_number)
-    for pos_propaged in laby.krustal_cell[str(other_number)]:
-        laby.board[pos_propaged[0]][pos_propaged[1]].k_number = number
-        laby.krustal_cell[str(number)].append([pos_propaged[0],pos_propaged[1]])
-        laby.krustal_cell[str(other_number)].remove([pos_propaged[0],pos_propaged[1]])
-    return laby
 
+    for pos_propaged in laby.kruskal_cell[str(other_number)]:
+
+        laby.board[pos_propaged[0]][pos_propaged[1]].k_number = number
+
+        laby.kruskal_cell[str(number)].append([pos_propaged[0],pos_propaged[1]])
+
+    laby.kruskal_cell[str(other_number)] = []
+    
+    
 
 
 def getDirection(x1:int,y1:int,x2:int,y2:int) -> str:
--- Test/Generator/kruskalGenerator_test.py
+++ Test/Generator/kruskalGenerator_test.py
@@ -6,12 +6,25 @@ from Utils.utils import *
 class TestKruskalGenerator(unittest.TestCase):
 
     def test_backtrackGenerator(self):
-        Lab2 = kruskalGenerator(6,"kruskal_lab_test.txt")
+        Lab2 = kruskalGenerator(7,"kruskal_lab_test.txt")
         self.assertEqual(read_txt("kruskal_lab_test.txt")[0:2],"\n#")
         Lab1 = txt_to_labyrinth("kruskal_lab_test.txt")
         self.assertEqual(Lab1.board[0][0].walls["Top"],True)
-        self.assertEqual(Lab1.length,6)
-        self.assertEqual(Lab2.board[2][0].k_number, 0)
+        self.assertEqual(Lab1.length,7)
+        self.assertEqual(Lab2.board[2][5].k_number, 0)
+        self.assertEqual(len(Lab2.kruskal_cell["0"]),49)
+        self.assertNotEqual(Lab2.board[3][4].walls,{"Top":True,"Bottom":True,"Left":True,"Right":True})
 
+    def test_propagation(self):
+        Lab1 = Labyrinth(3)
+        propagation(Lab1,[0,1],[0,2])
+        self.assertEqual(Lab1.kruskal_cell["1"],[[0,1],[0,2]])
+        self.assertEqual(Lab1.board[0][1].walls["Right"],False)
+        self.assertEqual(Lab1.board[0][2].k_number,1)
+        self.assertEqual(Lab1.kruskal_cell["2"],[])
+        propagation(Lab1, [0,0],[0,1])
+        self.assertEqual(Lab1.board[0][2].k_number,0)
+        self.assertEqual(Lab1.kruskal_cell["0"],[[0,0],[0,1],[0,2]])
+        self.assertEqual(Lab1.kruskal_cell["1"],[])
 
         
